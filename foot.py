cd6651126b879167c0d339f28ee723f7e54cfd46
Patatel <lea.regoudis@gmail.com>
2024-01-11 11:07:23 +0100

    Nv1 - Fc3

--- foot.py
+++ foot.py
@@ -2,7 +2,7 @@
 nomFichier = 'FIFA-2022.txt'
 
 # 1_Récupere les donner du fichier en liste
-def recup_liste(fichier):
+def charger_donnees(fichier):
     # Ouvre le fichier en mode lecture
     with open(fichier, 'r', encoding='utf-8') as file:
         # Lit le contenu du fichier
@@ -23,12 +23,27 @@ def recup_liste(fichier):
         chaine_split = liste_chaine.split(',')
         liste_fin.append(chaine_split)
 
-    # Suppression de la première ligne
+    # 2_Suppression de la première ligne
     del(liste_fin[0])
 
     # Return le résultat
     return liste_fin
 
+liste_matchs = charger_donnees(nomFichier)
 
-liste = recup_liste(nomFichier)
-print(liste)
+# 3_Afficher pays avec seulement 3 lettres
+def afficher_nom_trois_lettres(liste) :
+    # Initialisation de la liste des pays en 3 lettres
+    p3 = []
+
+    # Parcourrir la liste
+    for i in range(len(liste)) :
+        pays = liste[i][1]
+        lettres = pays[0:3]
+        p3.append(lettres.upper())
+
+    # Return le résultat
+    return p3
+
+paysXXX = afficher_nom_trois_lettres(liste_matchs)
+print(paysXXX)
